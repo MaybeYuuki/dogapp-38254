d6fae48bd921ec80689b3f816c37f63c4dc0935a
MaybeYuuki <baseballff13@gmail.com>
2022-08-24 20:50:13 +0900

    ユーザー情報を保存できる処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,
+                                             :birth_date, :postal_code, :area_id, :municipalities, :address, :building_name])
+  end
 end
