70348ada1ba7a1f0d2a3414772346087518fbe40
MaybeYuuki <baseballff13@gmail.com>
2022-08-25 15:43:25 +0900

    カラムの設定

--- db/migrate/20220824132744_create_dogs.rb
+++ db/migrate/20220824132744_create_dogs.rb
@@ -1,7 +1,16 @@
 class CreateDogs < ActiveRecord::Migration[6.0]
   def change
     create_table :dogs do |t|
-
+      t.string :name,            null: false
+      t.integer :price,          null: false
+      t.integer :gender_id,      null: false
+      t.text :explanation,       null: false
+      t.integer :hair_color,     null: false
+      t.integer :area_id,        null: false
+      t.integer :birth_date_id,  null: false
+      t.integer :height,         null: false
+      t.integer :wight,          null: false
+      t.references :user,        null: false, foreign_key: true
       t.timestamps
     end
   end
---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_08_24_132744) do
+
+  create_table "dogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "gender_id", null: false
+    t.text "explanation", null: false
+    t.integer "hair_color", null: false
+    t.integer "area_id", null: false
+    t.integer "birth_date_id", null: false
+    t.integer "height", null: false
+    t.integer "wight", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth_date", null: false
+    t.string "postal_code", null: false
+    t.integer "area_id", null: false
+    t.string "municipalities", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "dogs", "users"
+end
