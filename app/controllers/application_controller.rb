7dfcc19f278b3fdcd774ba56fea5f192399c9fe7
MaybeYuuki <baseballff13@gmail.com>
2022-08-30 21:22:05 +0900

    ストロングパロメータの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,
                                              :birth_date, :postal_code, :area_id, :municipalities, :address, :building_name])
   end
 end
